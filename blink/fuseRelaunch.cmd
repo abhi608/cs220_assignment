252339faa23c8775aa2141c39f7e0f3929b826c1
Deepak Gangwar <deeepakg1998@gmail.com>
2017-03-10 03:12:43 +0530

    done 1st question of verilog - blink

---
+++ blink/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/home/deepak/6th sem/CS220A/Assignments/blink/blinkTest_isim_beh.exe" -prj "/home/deepak/6th sem/CS220A/Assignments/blink/blinkTest_beh.prj" "work.blinkTest" "work.glbl" 
