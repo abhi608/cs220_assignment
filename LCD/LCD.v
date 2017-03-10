ac403dc28e75e25ebafbac4191bb77f166c537ef
Deepak Gangwar <deeepakg1998@gmail.com>
2017-03-10 15:25:54 +0530

    completed /LCD - changed hello world to computer orgainzation

--- LCD/LCD.v
+++ LCD/LCD.v
@@ -65,34 +65,96 @@ case (count [26:21])	// as top 6 bits change
 // Chararters are then given out, the cursor will advance to the right 
 // Table 5-3, Write Data to DD RAM (or CG RAM) 
 // Fig 5-4, 'H' send 10 and upper nibble 0100, then 10 and lower nibble 
-12: code <= 6'h24; // 'H' high nibble 
-13: code <= 6'h28; // 'H' low nibble 
-14: code <= 6'h26; // e 
-15: code <= 6'h25; 
-16: code <= 6'h26; // 1 
-17: code <= 6'h2C; 
-18: code <= 6'h26; // 1 
-19: code <= 6'h2C;
-20: code <= 6'h26; // 0 
-21: code <= 6'h2F; 
-22: code <= 6'h22; // , 
-23: code <= 6'h2C; // Table 5-3, Set DD RAM (DDR) Address 
+//12: code <= 6'h24; // 'H' high nibble 
+//13: code <= 6'h28; // 'H' low nibble 
+//14: code <= 6'h26; // e 
+//15: code <= 6'h25; 
+//16: code <= 6'h26; // 1 
+//17: code <= 6'h2C; 
+//18: code <= 6'h26; // 1 
+//19: code <= 6'h2C;
+//20: code <= 6'h26; // 0 
+//21: code <= 6'h2F; 
+//22: code <= 6'h22; // , 
+//23: code <= 6'h2C; // Table 5-3, Set DD RAM (DDR) Address 
 // position the cursor onto the start of the 2nd line 
 // send 00 and upper nibble 1???, ??? is the highest 3 bits of the DDR 
 // address to move the cursor to, then 00 and lower 4 bits of the addr 
 // so ??? is 100 and then 0000 for h40 
-24: code <= 6'b001100; // pos cursor to 2nd line upper nibble 
-25: code <= 6'b000000; // lower nibble: h0 
+//24: code <= 6'b001100; // pos cursor to 2nd line upper nibble 
+//25: code <= 6'b000000; // lower nibble: h0 
 // Characters are then given out, the cursor will advance to the right 
-26: code <= 6'h25; // W 
-27: code <= 6'h27; 28: code <= 6'h26; // o 
-29: code <= 6'h2F; 30: code <= 6'h27; // r 
-31: code <= 6'h22; 32: code <= 6'h26; // l 
-33: code <= 6'h2C; 34: code <= 6'h26; // d 
-35: code <= 6'h24; 36: code <= 6'h22; // ! 
-37: code <= 6'h21; // Table 5-3, Read Busy Flag and Address 
+//26: code <= 6'h25; // W 
+//27: code <= 6'h27; 28: code <= 6'h26; // o 
+//29: code <= 6'h2F; 30: code <= 6'h27; // r 
+//31: code <= 6'h22; 32: code <= 6'h26; // l 
+//33: code <= 6'h2C; 34: code <= 6'h26; // d 
+//35: code <= 6'h24; 36: code <= 6'h22; // ! 
+//37: code <= 6'h21; // Table 5-3, Read Busy Flag and Address 
 // send 01 BF (Busy Flag) x x x, then 01xxxx 
 // idling
+ 
+ 
+12: code <= 6'h24; // 'C' high nibble 
+13: code <= 6'h23; // 'C' low nibble 
+14: code <= 6'h26; // o 
+15: code <= 6'h2F; 
+16: code <= 6'h26; // m 
+17: code <= 6'h2D; 
+18: code <= 6'h27; // p 
+19: code <= 6'h20;
+20: code <= 6'h27; // u 
+21: code <= 6'h25; 
+22: code <= 6'h27; // t 
+23: code <= 6'h24; 
+24: code <= 6'h26; // e 
+25: code <= 6'h25; 
+26: code <= 6'h27; // r 
+27: code <= 6'h22; 
+
+
+
+
+
+// Table 5-3, Set DD RAM (DDR) Address 
+// position the cursor onto the start of the 2nd line 
+// send 00 and upper nibble 1???, ??? is the highest 3 bits of the DDR 
+// address to move the cursor to, then 00 and lower 4 bits of the addr 
+// so ??? is 100 and then 0000 for h40 
+28: code <= 6'b001100; // pos cursor to 2nd line upper nibble 
+29: code <= 6'b000000; // lower nibble: h0 
+// Characters are then given out, the cursor will advance to the right 
+30: code <= 6'h24; // O 
+31: code <= 6'h2F;
+32: code <= 6'h26; // r 
+33: code <= 6'h22; 
+34: code <= 6'h26; // g 
+35: code <= 6'h27; 
+36: code <= 6'h26; // a 
+37: code <= 6'h21; 
+38: code <= 6'h26; // n 
+39: code <= 6'h2E; 
+40: code <= 6'h26; // i 
+41: code <= 6'h29; 
+42: code <= 6'h27; // z 
+43: code <= 6'h2A; 
+44: code <= 6'h26; // a 
+45: code <= 6'h21; 
+46: code <= 6'h27; // t 
+47: code <= 6'h24; 
+48: code <= 6'h26; // i 
+49: code <= 6'h29; 
+50: code <= 6'h26; // o 
+51: code <= 6'h2F; 
+52: code <= 6'h26; // n 
+53: code <= 6'h2E; 
+
+
+// Table 5-3, Read Busy Flag and Address 
+// send 01 BF (Busy Flag) x x x, then 01xxxx 
+// idling
+ 
+ 
  
 default: code <= 6'h10; // the restun-used time 
 endcase 
